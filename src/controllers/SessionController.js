5a32118d7e02bd12bfa63b4319939e92ca815e19
Adriann Guedes <adrianguedes902@gmail.com>
2023-09-03 21:26:58 -0300

    DevHouse

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,40 @@
+//métodos:index, show, update, store, destroy
+/*
+index: listagem de sessoes
+store: criar uma sessao
+show: listar uma unica sessao
+update: quando queremos alterar alguma sessao
+destroy: quando queremos deletar uma sessao
+
+*/
+
+import User from '../models/User';
+import * as Yup from 'yup';
+
+class SessionController {
+
+    async store(req, res) {
+        const schema = Yup.object().shape({
+            email: Yup.string().email().required(),
+        });
+
+
+        const { email } = req.body;
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Falha na validação.' });
+        }
+
+        //Verificando se o usuário ja existe
+        let user = await User.findOne({ email });
+
+        if (!user) {
+            user = await User.create({ email });
+
+        }
+        return res.json(user);
+    }
+
+}
+
+export default new SessionController();
